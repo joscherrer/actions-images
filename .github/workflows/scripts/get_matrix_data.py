eec4b952f4cd2961ead7c8349d7ad53ffa4f231f
Jonathan Scherrer <jonathan.s.scherrer@gmail.com>
2022-11-02 18:49:14 +0100

    python image

---
+++ .github/workflows/scripts/get_matrix_data.py
@@ -0,0 +1,19 @@
+import os
+import json
+from pathlib import Path
+
+
+def write_output(var, value):
+    if GITHUB_OUTPUT := os.getenv("GITHUB_OUTPUT"):
+        with Path(GITHUB_OUTPUT).open("a") as f:
+            f.write(f"{var}={value}\n")
+    else:
+        print(f"::set-output name={var}::{value}")
+
+
+container_files = list(Path().glob("*.Containerfile"))
+data = [{"tag": x.stem, "file": x.name} for x in container_files]
+
+write_output("matrix_data", json.dumps(data))
+
+exit(0)
